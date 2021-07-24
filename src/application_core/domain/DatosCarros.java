2b3bdc42a4e543f6fc99d242f5914fe121ea1b4f
karlos6 <Carlos.Keller.M@gmail.com>
2021-07-23 19:20:59 -0500

    Creacion metodos de la clase DatosCarros

--- src/application_core/domain/DatosCarros.java
+++ src/application_core/domain/DatosCarros.java
@@ -1,5 +1,21 @@
 package application_core.domain;
+import java.util.ArrayList;
+import java.util.List;
+
+
 
 public class DatosCarros {
+    private List<Carro> carros = new ArrayList<Carro>(); 
+
+    public void registrarCarro(Carro carro){
+
+    }
+
+    public List<Carro> consultarCarros(){
+
+
+        return carros;  
+
+    }
     
 }
