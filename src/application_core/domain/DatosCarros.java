59e5a7adbc4a3ebc22e656d29f8be7b739603907
karlos6 <Carlos.Keller.M@gmail.com>
2021-07-23 19:38:29 -0500

    implemnetacion del codigo en los metodos registrarCarro y ConsultarCarros

--- src/application_core/domain/DatosCarros.java
+++ src/application_core/domain/DatosCarros.java
@@ -8,14 +8,11 @@ public class DatosCarros {
     private List<Carro> carros = new ArrayList<Carro>(); 
 
     public void registrarCarro(Carro carro){
-
+        carros.add(carro);
     }
 
     public List<Carro> consultarCarros(){
-
-
-        return carros;  
-
+        return carros;
     }
     
 }
